42a342d754deb77bfe12ef911578be39ea266d55
Song <quanpengsong@gmail.com>
2021-01-16 09:50:46 +0800

    add star_watcher

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Scripts
+## Star Watcher
+用于监控 Github Star 数量，发现有变化就发送消息通知。
+
+需要配合 cron 使用。
+
+使用方法：
+`.\star_watcher.py GITHUB_USERNAME GITHUB_TOKEN  https://sc.ftqq.com/[SCKEY].send?text=`
+第三个参数是消息推送 URL，上述给的例子是 server 酱，也可以用[我自己实现的版本](https://github.com/songquanpeng/message-pusher)。
---
+++ star_watcher.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import requests
+import os
+import sys
+
+filename = "./.star_count"
+push_url = ""
+
+
+def star_counter(username: str, token: "") -> int:
+    all_repos_url = f"https://api.github.com/users/{username}/repos?per_page=100"
+    header = {} if token == "" else {"Authorization": f"bearer {token}"}
+    res = requests.get(all_repos_url, header)
+    repos = res.json()
+    count = 0
+    for repo in repos:
+        count += repo["stargazers_count"]
+    return count
+
+
+def load_count() -> int:
+    try:
+        with open(filename, 'r') as f:
+            count = int(f.read())
+    except IOError:
+        count = 0
+    return count
+
+
+def save_count(count: int):
+    with open(filename, 'w') as f:
+        f.write(str(count))
+
+
+def send_message(msg: str):
+    requests.get(f"{push_url}{msg}")
+
+
+def main():
+    if len(sys.argv) != 4:
+        print(f"Error! This script requires three arguments: GITHUB_USERNAME GITHUB_TOKEN PUSH_URL")
+        return
+    username = sys.argv[1]
+    token = sys.argv[2]
+    global push_url
+    push_url = sys.argv[3]
+    last_count = load_count()
+    current_count = star_counter(username, token)
+    if current_count != last_count:
+        if current_count > last_count:
+            send_message(f"Star 数量增加了，哈哈！{last_count} --> {current_count}！")
+        else:
+            send_message(f"Star 数量减少了，淦！{last_count} --> {current_count}！")
+        save_count(current_count)
+
+
+if __name__ == '__main__':
+    main()
